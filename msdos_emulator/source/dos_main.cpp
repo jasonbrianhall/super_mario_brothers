76f3c6b01a35243bd737cff7c0cd63ed6f3d759d
Jason Hall <jasonbrianhall@gmail.com>
2025-07-18 15:08:25 -0400

    Latest

--- msdos_emulator/source/dos_main.cpp
+++ msdos_emulator/source/dos_main.cpp
@@ -1101,12 +1101,7 @@ void AllegroMainWindow::run(const char* romFilename)
         updateAndDraw();
 
         #ifdef __DJGPP__
-        while (true) {
-            unsigned long nowTicks = uclock();
-            double elapsedMs = (nowTicks - lastTicks) / ticksPerMs;
-            if (elapsedMs >= targetFrameMs) break;
-        }
-        lastTicks = uclock();  // Reset frame tick
+        vsync();
         #else
         auto now = std::chrono::high_resolution_clock::now();
         std::chrono::duration<double, std::milli> elapsed = now - lastFrameTime;
